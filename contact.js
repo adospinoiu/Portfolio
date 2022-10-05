01c1fb4d46d066e73f01f33093830c5ca1441410
adospinoiu <alex.dospinoiu@gmail.com>
2022-10-04 20:55:45 -0700

    The email aspect works now from the front end to my eamil

--- contact.js
+++ contact.js
@@ -5,8 +5,36 @@ form.addEventListener('submit', event => {
     event.preventDefault()
 
     const { name, email, message } = event.target
+
     console.log('Name: ', name.value)
     console.log('email: ', email.value)
     console.log('Message: ', message.value)
+
+    const endpoint = "https://5y2xvzl6re.execute-api.us-west-1.amazonaws.com/Prod/sendContactEmail"
+
+    const body = JSON.stringify({
+        senderName: name.value,
+        senderEmail: email.value,
+        message: message.value
+    })
+
+    const requestOptions = {
+        method: "POST",
+        body
+    }
+
+    fetch(endpoint, requestOptions)
+        .then((response) => {
+            if (!response.ok) throw new Error("Error in fetch");
+            return response.json();
+        })
+        .then((response) => {
+            document.getElementById("result-text").innerText =
+                "Email sent successfully!";
+        })
+        .catch((error) => {
+            document.getElementById("result-text").innerText =
+                "An unkown error occured.";
+        });
 
 })
